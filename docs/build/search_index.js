ac85a5af5936645471b17d50c165567abe0d6bdc
davibarreira <davisbarreira@gmail.com>
2020-12-15 15:07:43 -0300

    Creating documents

---
+++ docs/build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#VegaGraphs-Documentation","page":"VegaGraphs Documentation","title":"VegaGraphs Documentation","text":"","category":"section"}]
+}
---
+++ docs/make.jl
@@ -0,0 +1,6 @@
+push!(LOAD_PATH,"../src/")
+using VegaGraphs
+
+using Documenter
+
+makedocs(sitename="VegaGraphs")
