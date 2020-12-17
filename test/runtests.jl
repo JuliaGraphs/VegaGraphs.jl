3c22ab8ec5d55f6d92ccbeba42e09c6237822407
davibarreira <davisbarreira@gmail.com>
2020-12-16 22:10:08 -0300

    Added test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,13 @@
-using DataFrames, Random
+using DataFrames, Random, VegaLite
 using VegaGraphs
 using Test
 
 @testset "graphplot()" begin
     Random.seed!(7)
-    df = DataFrame(rand([0,1],20,20));
-    field= rand([100,200,300],20)
-    VegaGraphs.graphplot(df,tooltip=true,
+    df    = DataFrame(rand([0,1],20,20));
+    field = rand([100,200,300],20)
+    p     = VegaGraphs.graphplot(df,tooltip=true,
         node_label=false,node_colorfield=field,
         node_colorfieldtype="o",node_sizefield=field)
-    @test 1 == 1
+    @test typeof(p) == VegaLite.VLSpec
 end
