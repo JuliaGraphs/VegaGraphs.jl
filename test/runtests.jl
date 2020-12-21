53cdf6702b8a96c2acebf6a4976427adbb160062
davibarreira <davisbarreira@gmail.com>
2020-12-21 15:29:58 -0300

    Fixed the test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,15 @@
-using DataFrames, Random, VegaLite
+using DataFrames, Random, VegaLite, SimpleWeightedGraphs, LinearAlgebra
 using VegaGraphs
 using Test
 
 @testset "graphplot()" begin
     Random.seed!(7)
-    df    = DataFrame(rand([0,1],20,20));
+    incidence = rand([0,1],20,20);
+    m         = incidence'*incidence
+    m[diagind(m)] .= 0.0
+    g = SimpleWeightedGraph(m)
     field = rand([100,200,300],20)
-    p     = VegaGraphs.graphplot(df,tooltip=true,
+    p     = VegaGraphs.graphplot(g,tooltip=true,
         node_label=false,node_colorfield=field,
         node_colorfieldtype="o",node_sizefield=field)
     @test typeof(p) == VegaLite.VLSpec
